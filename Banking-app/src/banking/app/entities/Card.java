988a4a9ebd3d58f752ab0b1044ab4e506704020d
Simon Mandlik <simon.mandlik@gmail.com>
2017-04-16 15:44:55 +0200

    card

--- Banking-app/src/banking/app/entities/Card.java
+++ Banking-app/src/banking/app/entities/Card.java
@@ -1,5 +1,71 @@
 package banking.app.entities;
 
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Card {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @Column(nullable = false)
+    private CardType cardType;    
+    
+    @ManyToOne
+    @Column(nullable = false)
+    private Account account;
+    
+    @Column(nullable = false)
+    private BigDecimal withdrawalLimit;
+
+    public Card(Long id, CardType cardType, Account account, BigDecimal withdrawalLimit) {
+        this.id = id;
+        this.cardType = cardType;
+        this.account = account;
+        this.withdrawalLimit = withdrawalLimit;
+    }
+
+    public Card() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(CardType cardType) {
+        this.cardType = cardType;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public BigDecimal getWithdrawalLimit() {
+        return withdrawalLimit;
+    }
+
+    public void setWithdrawalLimit(BigDecimal withdrawalLimit) {
+        this.withdrawalLimit = withdrawalLimit;
+    }    
     
 }
--- Banking-app/src/banking/app/entities/CardType.java
+++ Banking-app/src/banking/app/entities/CardType.java
@@ -1,5 +1,54 @@
 package banking.app.entities;
 
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class CardType {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(nullable = false, unique = true, length = 100)
+    private String typeName;
+    
+    @OneToMany(mappedBy = "cardType")
+    private List<Card> cards;
+
+    public CardType() {
+    }
+    
+    public CardType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }    
 }
